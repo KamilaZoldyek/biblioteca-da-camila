3e461de2054c5f8749d8ce1be394667e73f9a4e5
KamilaZoldyek <kmilazd@gmail.com>
2025-08-21 14:02:33 -0300

    feat: aboutScreen pronta com os deeplinks

---
+++ components/CustomCard/CustomCard.tsx
@@ -0,0 +1,40 @@
+import { Colors, Dimensions } from "@/constants/";
+import * as React from "react";
+import { Pressable, StyleProp, ViewStyle } from "react-native";
+import { Card, Icon } from "react-native-paper";
+
+type CustomCardProps = {
+  customStyle?: StyleProp<ViewStyle>;
+  title: string;
+  subtitle: string;
+  iconName: string;
+  theme: string | null;
+  onPress?: () => void;
+};
+
+export default function CustomCard({
+  customStyle,
+  title,
+  subtitle,
+  iconName,
+  theme,
+  onPress,
+}: CustomCardProps) {
+  return (
+    <Pressable onPress={onPress} style={{ width: "100%" }}>
+      <Card.Title
+        style={{
+          backgroundColor:
+            theme === "dark"
+              ? Colors.dark.inverseOnSurface
+              : Colors.light.inverseOnSurface,
+          borderRadius: 10,
+          marginVertical: Dimensions.padding.halfContainer,
+        }}
+        title={title}
+        subtitle={subtitle}
+        left={(props) => <Icon source={iconName} size={35} />}
+      />
+    </Pressable>
+  );
+}
