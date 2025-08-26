dd53f10691e2d48e947d17d90f6c7ed300936d77
KamilaZoldyek <kmilazd@gmail.com>
2025-08-25 22:06:13 -0300

    feat: section list arrumada e adicionei o fab

--- components/TagList/TagList.tsx
+++ components/TagList/TagList.tsx
@@ -1,16 +1,22 @@
 import { Colors } from "@/constants";
 import { TAGS } from "@/constants/mocks";
 import * as React from "react";
+import { useEffect, useState } from "react";
 import { FlatList, StyleProp, StyleSheet, ViewStyle } from "react-native";
 import { Chip } from "react-native-paper";
 
 type TagListProps = {
   customStyle?: StyleProp<ViewStyle>;
   onPress: (selected: string) => void;
+  clearSelection?: boolean;
 };
 
-export default function TagList({ customStyle, onPress }: TagListProps) {
-  const [selected, setSelected] = React.useState("");
+export default function TagList({
+  customStyle,
+  onPress,
+  clearSelection,
+}: TagListProps) {
+  const [selected, setSelected] = useState("");
 
   const ripple = {
     color: Colors.dark.primary,
@@ -19,13 +25,20 @@ export default function TagList({ customStyle, onPress }: TagListProps) {
     borderless: false,
   };
 
+  useEffect(() => {
+    if (clearSelection) {
+      setSelected("");
+    }
+  }, [clearSelection, onPress]);
+
   return (
     <FlatList
       horizontal
+      showsHorizontalScrollIndicator={false}
       data={TAGS}
       renderItem={({ item }) => (
         <Chip
-          onPress={() => onPress(selected === item ? item : '')}
+          onPress={() => onPress(selected === item ? item : "")}
           onPressIn={() => setSelected(selected === item ? "" : item)}
           selected={selected === item}
           style={styles.chip}
