c0c78348221b6f0ab24db2005f3f961a99953b96
KamilaZoldyek <kmilazd@gmail.com>
2025-09-06 19:01:21 -0300

    feat: refactor do form

--- app/MetadataScreen.tsx
+++ app/MetadataScreen.tsx
@@ -13,6 +13,7 @@ import * as ImagePicker from "expo-image-picker";
 import { router, useFocusEffect, useLocalSearchParams } from "expo-router";
 import * as React from "react";
 import { useCallback, useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import {
   BackHandler,
   Image,
@@ -43,24 +44,32 @@ export default function MetadataScreen() {
   const RATING_LIST = ["N/A", "1", "2", "3", "4", "5"];
   //TODO isso provavelmente vai virar um type, assim como os outros
 
-  const [bookTitle, setBookTitle] = useState("");
-  const [collectionTitle, setCollectionTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const [volume, setVolume] = useState("");
-  const [publisher, setPublisher] = useState("");
-  const [hasSynopsis, setHasSynopsis] = useState(true);
-  const [synopsis, setSynopsis] = useState("");
-  const [year, setYear] = useState("");
-  const [wasRead, setWasRead] = useState("Não lido");
-  const [kind, setKind] = useState("Mangá");
-  const [location, setLocation] = useState("Minha casa");
-  const [collectionStatus, setCollectionStatus] = useState("");
-  const [uploadCoverName, setUploadCoverName] = useState<string | null>(null);
+  const { control, handleSubmit, watch, formState } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      bookTitle: "",
+      collectionTitle: "",
+      author: "",
+      volume: "",
+      isbn: ISBN,
+      publisher: "",
+      synopsis: "",
+      year: "",
+      review: "",
+      readingStatus: "Não lido",
+      kind: "Mangá",
+      location: "Minha casa",
+      collectionStatus: "Incompleta",
+      rating: "N/A",
+    },
+  });
+
   const [hasReview, setHasReview] = useState(true);
-  const [review, setReview] = useState("");
+  const [hasSynopsis, setHasSynopsis] = useState(true);
+
+  const [uploadCoverName, setUploadCoverName] = useState<string | null>(null);
+
   const [theme, setTheme] = useState<"light" | "dark" | null>(null);
-  const [disabled, setDisabled] = useState(false);
-  const [rating, setRating] = useState("");
   const [showWebview, setShowWebview] = useState(false);
   const [showGoBackModal, setShowGoBackModal] = useState(false);
   const [showLoading, setShowLoading] = useState(false);
@@ -85,14 +94,6 @@ export default function MetadataScreen() {
       setTheme(mode);
     });
   }, [colorScheme, setTheme]);
-
-  useEffect(() => {
-    if (bookTitle === "" || collectionTitle === "") {
-      setDisabled(true);
-    } else {
-      setDisabled(false);
-    }
-  }, [bookTitle, collectionTitle]);
 
   useFocusEffect(
     useCallback(() => {
@@ -133,195 +134,273 @@ export default function MetadataScreen() {
     router.navigate("/HomeScreen");
   };
 
-  const handleSave = () => {
-    console.log("click");
-    // TODO
+  const handleSave = (data: any) => {
+    console.log(data);
+    // TODO adicionar a URL da capa aqui no data
   };
 
   const renderTitleBlock = () => {
     return (
-      <View style={styles.textInputs}>
-        <TextInput
-          label={Strings.metadataScreen.bookTitle}
-          value={bookTitle}
-          onChangeText={(text) => setBookTitle(text)}
-          mode="outlined"
-          error={bookTitle === ""}
-        />
-        <HelperText style={styles.textInputs} type={"info"} visible={true}>
-          {Strings.metadataScreen.bookTitleHelper}
-        </HelperText>
-      </View>
+      <Controller
+        rules={{ required: Strings.metadataScreen.bookTitleHelper }}
+        control={control}
+        name={"bookTitle"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.textInputs}>
+            <TextInput
+              label={Strings.metadataScreen.bookTitle}
+              value={value}
+              onChangeText={onChange}
+              mode="outlined"
+              error={value === ""}
+            />
+            <HelperText style={styles.textInputs} type={"info"} visible={true}>
+              {Strings.metadataScreen.bookTitleHelper}
+            </HelperText>
+          </View>
+        )}
+      />
     );
   };
 
   const renderCollectionBlock = () => {
     return (
-      <View style={styles.textInputs}>
-        <TextInput
-          label={Strings.metadataScreen.collectionTitle}
-          value={collectionTitle}
-          onChangeText={(text) => setCollectionTitle(text)}
-          mode="outlined"
-          error={bookTitle === ""}
-        />
-        <HelperText style={styles.textInputs} type={"info"} visible={true}>
-          {Strings.metadataScreen.collectionTitleHelper}
-        </HelperText>
-      </View>
+      <Controller
+        rules={{ required: Strings.metadataScreen.collectionTitleHelper }}
+        control={control}
+        name={"collectionTitle"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.textInputs}>
+            <TextInput
+              label={Strings.metadataScreen.collectionTitle}
+              value={value}
+              onChangeText={onChange}
+              mode="outlined"
+              error={value === ""}
+            />
+            <HelperText style={styles.textInputs} type={"info"} visible={true}>
+              {Strings.metadataScreen.collectionTitleHelper}
+            </HelperText>
+          </View>
+        )}
+      />
     );
   };
 
   const renderAuthorBlock = () => {
     return (
-      <View style={styles.textInputs}>
-        <TextInput
-          label={Strings.metadataScreen.author}
-          value={author}
-          onChangeText={(text) => setAuthor(text)}
-          mode="outlined"
-        />
-        <HelperText style={styles.textInputs} type={"info"} visible={true}>
-          {Strings.metadataScreen.authorHelper}
-        </HelperText>
-      </View>
+      <Controller
+        control={control}
+        name={"author"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.textInputs}>
+            <TextInput
+              label={Strings.metadataScreen.author}
+              value={value}
+              onChangeText={onChange}
+              mode="outlined"
+            />
+            <HelperText style={styles.textInputs} type={"info"} visible={true}>
+              {Strings.metadataScreen.authorHelper}
+            </HelperText>
+          </View>
+        )}
+      />
     );
   };
 
   const renderVolumeBlock = () => {
     return (
-      <View style={styles.textInputs}>
-        <TextInput
-          label={Strings.metadataScreen.volume}
-          value={volume.toString()}
-          keyboardType="numeric"
-          onChangeText={(text) => setVolume(text)}
-          mode="outlined"
-        />
-        <HelperText style={styles.textInputs} type={"info"} visible={true}>
-          {Strings.metadataScreen.volumeHelper}
-        </HelperText>
-      </View>
+      <Controller
+        control={control}
+        name={"volume"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.textInputs}>
+            <TextInput
+              label={Strings.metadataScreen.volume}
+              value={value.toString()}
+              keyboardType="numeric"
+              onChangeText={onChange}
+              mode="outlined"
+            />
+            <HelperText style={styles.textInputs} type={"info"} visible={true}>
+              {Strings.metadataScreen.volumeHelper}
+            </HelperText>
+          </View>
+        )}
+      />
     );
   };
 
   const renderISBNBlock = () => {
     return (
-      <View style={styles.textInputs}>
-        <TextInput
-          label={Strings.metadataScreen.isbn}
-          value={ISBN}
-          mode="outlined"
-          editable={false}
-        />
-        <HelperText style={styles.textInputs} type={"info"} visible={true}>
-          {Strings.metadataScreen.isbnHelper}
-        </HelperText>
-      </View>
+      <Controller
+        control={control}
+        name={"isbn"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.textInputs}>
+            <TextInput
+              label={Strings.metadataScreen.isbn}
+              value={ISBN}
+              mode="outlined"
+              editable={false}
+            />
+            <HelperText style={styles.textInputs} type={"info"} visible={true}>
+              {Strings.metadataScreen.isbnHelper}
+            </HelperText>
+          </View>
+        )}
+      />
     );
   };
 
   const renderPublisherBlock = () => {
     return (
-      <View style={styles.textInputs}>
-        <TextInput
-          style={styles.mediumMarginBottom}
-          label={Strings.metadataScreen.publisher}
-          value={publisher}
-          onChangeText={(text) => setPublisher(text)}
-          mode="outlined"
-        />
-      </View>
+      <Controller
+        control={control}
+        name={"publisher"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.textInputs}>
+            <TextInput
+              style={styles.mediumMarginBottom}
+              label={Strings.metadataScreen.publisher}
+              value={value}
+              onChangeText={onChange}
+              mode="outlined"
+            />
+          </View>
+        )}
+      />
     );
   };
 
   const renderYearBlock = () => {
     return (
-      <View style={styles.textInputs}>
-        <TextInput
-          label={Strings.metadataScreen.year}
-          value={year}
-          onChangeText={(text) => setYear(text)}
-          mode="outlined"
-        />
-      </View>
+      <Controller
+        control={control}
+        name={"year"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.textInputs}>
+            <TextInput
+              label={Strings.metadataScreen.year}
+              value={value}
+              onChangeText={onChange}
+              mode="outlined"
+            />
+          </View>
+        )}
+      />
     );
   };
 
   const renderSynopsisBlock = () => {
     return (
-      <View style={styles.blocks}>
-        <Text variant="titleMedium">{Strings.metadataScreen.synopsis}</Text>
-        <Chip
-          style={styles.chip}
-          icon={!hasSynopsis ? "check" : undefined}
-          mode={!hasSynopsis ? "flat" : "outlined"}
-          onPress={() => setHasSynopsis(!hasSynopsis)}
-          disabled={synopsis !== ""}
-        >
-          {Strings.metadataScreen.noSynopsis}
-        </Chip>
-        <TextInput
-          disabled={!hasSynopsis}
-          label={
-            !hasSynopsis
-              ? Strings.metadataScreen.noSynopsisReally
-              : Strings.metadataScreen.synopsis
-          }
-          value={synopsis}
-          onChangeText={(text) => setSynopsis(text)}
-          mode="outlined"
-          multiline
-        />
-      </View>
+      <Controller
+        control={control}
+        name={"synopsis"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.blocks}>
+            <Text variant="titleMedium">{Strings.metadataScreen.synopsis}</Text>
+            <Chip
+              style={styles.chip}
+              icon={!hasSynopsis ? "check" : undefined}
+              mode={!hasSynopsis ? "flat" : "outlined"}
+              onPress={() => setHasSynopsis(!hasSynopsis)}
+              disabled={value !== ""}
+            >
+              {Strings.metadataScreen.noSynopsis}
+            </Chip>
+            <TextInput
+              disabled={!hasSynopsis}
+              label={
+                !hasSynopsis
+                  ? Strings.metadataScreen.noSynopsisReally
+                  : Strings.metadataScreen.synopsis
+              }
+              value={value}
+              onChangeText={onChange}
+              mode="outlined"
+              multiline
+            />
+          </View>
+        )}
+      />
     );
   };
 
   const renderReadingStatusBlock = () => {
     return (
-      <View style={styles.blocks}>
-        <ChipsWithTitle
-          onPress={(status) => setWasRead(status)}
-          data={["Não lido", "Lido"]}
-          title={Strings.metadataScreen.readingStatus}
-        />
-      </View>
+      <Controller
+        control={control}
+        name={"readingStatus"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.blocks}>
+            <ChipsWithTitle
+              onPress={onChange}
+              data={["Não lido", "Lido"]}
+              title={Strings.metadataScreen.readingStatus}
+              defaultValue={value}
+            />
+          </View>
+        )}
+      />
     );
   };
 
   const renderKindBlock = () => {
     return (
-      <View style={styles.blocks}>
-        <ChipsWithTitle
-          onPress={(kind) => setKind(kind)}
-          data={["Mangá", "Livro", "HQ"]}
-          title={Strings.metadataScreen.bookKind}
-        />
-      </View>
+      <Controller
+        control={control}
+        name={"kind"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.blocks}>
+            <ChipsWithTitle
+              onPress={onChange}
+              data={["Mangá", "Livro", "HQ"]}
+              title={Strings.metadataScreen.bookKind}
+              defaultValue={value}
+            />
+          </View>
+        )}
+      />
     );
   };
 
   const renderLocationBlock = () => {
     return (
-      <View style={styles.blocks}>
-        <ChipsWithTitle
-          onPress={(location) => setLocation(location)}
-          data={["Casa de mãe", "Minha casa"]}
-          title={Strings.metadataScreen.where}
-        />
-      </View>
+      <Controller
+        control={control}
+        name={"location"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.blocks}>
+            <ChipsWithTitle
+              onPress={onChange}
+              data={["Casa de mãe", "Minha casa"]}
+              title={Strings.metadataScreen.where}
+              defaultValue={value}
+            />
+          </View>
+        )}
+      />
     );
   };
 
   const collectionStatusBlock = () => {
     return (
-      <View style={styles.blocks}>
-        <ChipsWithTitle
-          onPress={(collectionStatus) => setCollectionStatus(collectionStatus)}
-          data={["Incompleta", "Completa"]}
-          title={Strings.metadataScreen.collectionStatus}
-        />
-      </View>
+      <Controller
+        control={control}
+        name={"collectionStatus"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.blocks}>
+            <ChipsWithTitle
+              onPress={onChange}
+              data={["Incompleta", "Completa"]}
+              title={Strings.metadataScreen.collectionStatus}
+              defaultValue={value}
+            />
+          </View>
+        )}
+      />
     );
   };
 
@@ -355,44 +434,57 @@ export default function MetadataScreen() {
 
   const renderRatingBlock = () => {
     return (
-      <View style={styles.blocks}>
-        <View style={styles.chipBlock}></View>
-        <ChipsWithTitle
-          onPress={(rating) => setRating(rating)}
-          data={RATING_LIST}
-          isRating
-          title={Strings.metadataScreen.rating}
-        />
-      </View>
+      <Controller
+        control={control}
+        name={"rating"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.blocks}>
+            <View style={styles.chipBlock}></View>
+            <ChipsWithTitle
+              onPress={onChange}
+              data={RATING_LIST}
+              isRating
+              title={Strings.metadataScreen.rating}
+              defaultValue={value}
+            />
+          </View>
+        )}
+      />
     );
   };
 
   const renderReviewBlock = () => {
     return (
-      <View style={styles.blocks}>
-        <Text variant="titleMedium">{Strings.metadataScreen.review}</Text>
-        <Chip
-          style={styles.chip}
-          icon={!hasReview ? "check" : undefined}
-          mode={!hasReview ? "flat" : "outlined"}
-          onPress={() => setHasReview(!hasReview)}
-          disabled={review !== ""}
-        >
-          {Strings.metadataScreen.noReview}
-        </Chip>
-        <TextInput
-          disabled={!hasReview}
-          label={
-            !hasReview
-              ? Strings.metadataScreen.noReviewReally
-              : Strings.metadataScreen.review
-          }
-          value={review}
-          onChangeText={(text) => setReview(text)}
-          mode="outlined"
-          multiline
-        />
-      </View>
+      <Controller
+        control={control}
+        name={"review"}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.blocks}>
+            <Text variant="titleMedium">{Strings.metadataScreen.review}</Text>
+            <Chip
+              style={styles.chip}
+              icon={!hasReview ? "check" : undefined}
+              mode={!hasReview ? "flat" : "outlined"}
+              onPress={() => setHasReview(!hasReview)}
+              disabled={value !== ""}
+            >
+              {Strings.metadataScreen.noReview}
+            </Chip>
+            <TextInput
+              disabled={!hasReview}
+              label={
+                !hasReview
+                  ? Strings.metadataScreen.noReviewReally
+                  : Strings.metadataScreen.review
+              }
+              value={value}
+              onChangeText={onChange}
+              mode="outlined"
+              multiline
+            />
+          </View>
+        )}
+      />
     );
   };
 
@@ -472,9 +564,9 @@ export default function MetadataScreen() {
 
               <LongButton
                 text={Strings.metadataScreen.save}
-                onPress={handleSave}
+                onPress={handleSubmit(handleSave)}
                 theme={theme}
-                disabled={disabled}
+                disabled={!formState.isValid}
               />
               <View style={{ paddingBottom: 20 }} />
             </ScrollView>
